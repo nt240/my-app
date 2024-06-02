2e33ad81e46de19ad1934b8dd3dcb0f3aa05c4c6
nt240 <tadashi.nishio@akane.waseda.jp>
2024-06-02 17:15:28 +0900

    fix

--- scripts/rewrite-paths.js
+++ scripts/rewrite-paths.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // パスを書き換えるディレクトリを指定します。
-const directoryPath = path.join(__dirname, '../out');
+const directoryPath = path.join(__dirname, '../src/app');
 
 fs.readdir(directoryPath, function (err, files) {
   if (err) {
@@ -15,13 +15,12 @@ fs.readdir(directoryPath, function (err, files) {
 
     // エントリがファイルであることを確認します。
     if (fs.statSync(filePath).isFile()) {
+    console.log('Processing file:', filePath);
       // ファイルの内容を読み込みます。
       let content = fs.readFileSync(filePath, 'utf8');
 
       // パスを書き換えます。
-      content = content.replace('/vercel.svg', '/my-app/vercel.svg');
-      content = content.replace('/next.svg', '/my-app/next.svg');
-
+      content = content.replace('src="/', 'src="/my-app/');
       // ファイルに書き込みます。
       fs.writeFileSync(filePath, content, 'utf8');
     }
